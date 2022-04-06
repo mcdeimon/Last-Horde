12cbe9ddb28bf3deb9ed9b3832d5dbd69807f426
“ArenasAgustin” <arenasagustin7@gmail.com>
2022-04-06 13:21:38 -0300

    footer

--- client/src/components/components/footer.js
+++ client/src/components/components/footer.js
@@ -1,95 +1,173 @@
-import React from 'react';
-import { Link } from '@reach/router';
+import React from "react";
+import { FaCopy } from "react-icons/fa";
 
-const footer= () => (
-  <footer className="footer-light">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-3 col-sm-6 col-xs-1">
-                        <div className="widget">
-                            <h5>Marketplace</h5>
-                            <ul>
-                                <li><Link to="">All NFTs</Link></li>
-                                <li><Link to="">Art</Link></li>
-                                <li><Link to="">Music</Link></li>
-                                <li><Link to="">Domain Names</Link></li>
-                                <li><Link to="">Virtual World</Link></li>
-                                <li><Link to="">Collectibles</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="col-md-3 col-sm-6 col-xs-1">
-                        <div className="widget">
-                            <h5>Resources</h5>
-                            <ul>
-                                <li><Link to="">Help Center</Link></li>
-                                <li><Link to="">Partners</Link></li>
-                                <li><Link to="">Suggestions</Link></li>
-                                <li><Link to="">Discord</Link></li>
-                                <li><Link to="">Docs</Link></li>
-                                <li><Link to="">Newsletter</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="col-md-3 col-sm-6 col-xs-1">
-                        <div className="widget">
-                            <h5>Community</h5>
-                            <ul>
-                                <li><Link to="">Community</Link></li>
-                                <li><Link to="">Documentation</Link></li>
-                                <li><Link to="">Brand Assets</Link></li>
-                                <li><Link to="">Blog</Link></li>
-                                <li><Link to="">Forum</Link></li>
-                                <li><Link to="">Mailing List</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="col-md-3 col-sm-6 col-xs-1">
-                        <div className="widget">
-                            <h5>Newsletter</h5>
-                            <p>Signup for our newsletter to get the latest news in your inbox.</p>
-                            <form action="#" className="row form-dark" id="form_subscribe" method="post" name="form_subscribe">
-                                <div className="col text-center">
-                                    <input className="form-control" id="txt_subscribe" name="txt_subscribe" placeholder="enter your email" type="text" /> 
-                                    <Link to="" id="btn-subscribe">
-                                      <i className="arrow_right bg-color-secondary"></i>
-                                    </Link>
-                                    <div className="clearfix"></div>
-                                </div>
-                            </form>
-                            <div className="spacer-10"></div>
-                            <small>Your email is safe with us. We don't spam.</small>
-                        </div>
-                    </div>
-                </div>
+const footer = () => {
+
+  const handleCopyClipboard = () => {
+    navigator.clipboard.writeText(
+      ``
+    );
+  };
+
+  return (
+    <footer className="footer-light">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3 col-sm-6 col-xs-1">
+            <div className="widget">
+              <img
+                width="205"
+                height="58"
+                src="/img/logolight.png"
+                alt="Logo"
+                className="footerLogo"
+              />
+
+              <p>
+                Last Horde is a fantasy card game based on NFT technology that
+                runs on the Binance Smart Chain (BSC) network. The game uses the
+                Unity engine to offer an immersive gaming experience where
+                players must face off in duels using their cards.
+              </p>
             </div>
-            <div className="subfooter">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="de-flex">
-                                <div className="de-flex-col">
-                                    <span onClick={()=> window.open("", "_self")}>
-                                        <img alt="" className="f-logo d-1" src="./img/logo.png" />
-                                        <img alt="" className="f-logo d-3" src="./img/logo-2-light.png" />
-                                        <img alt="" className="f-logo d-4" src="./img/logo-3.png" />
-                                        <span className="copy">&copy; Copyright 2021 - Gigaland by Designesia</span>
-                                    </span>
-                                </div>
-                                <div className="de-flex-col">
-                                    <div className="social-icons">
-                                        <span onClick={()=> window.open("", "_self")}><i className="fa fa-facebook fa-lg"></i></span>
-                                        <span onClick={()=> window.open("", "_self")}><i className="fa fa-twitter fa-lg"></i></span>
-                                        <span onClick={()=> window.open("", "_self")}><i className="fa fa-linkedin fa-lg"></i></span>
-                                        <span onClick={()=> window.open("", "_self")}><i className="fa fa-pinterest fa-lg"></i></span>
-                                        <span onClick={()=> window.open("", "_self")}><i className="fa fa-rss fa-lg"></i></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+          </div>
+
+          <div className="col-md-3 col-sm-6 col-xs-1">
+            <div className="widget">
+              <h3>LATEST NEWS</h3>
+
+              <ul>
+                <li>
+                  <a
+                    to="https://medium.com/@lasthorde/lets-talk-about-security-b94486011850"
+                    target="_blank"
+                  >
+                    Let’s talk about security?
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    to="https://medium.com/@lasthorde/next-steps-after-the-ido-833f65422676"
+                    target="_blank"
+                  >
+                    Next steps after the IDO
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    to="https://medium.com/@lasthorde/next-steps-after-the-ido-833f65422676"
+                    target="_blank"
+                  >
+                    About Last Horde
+                  </a>
+                </li>
+              </ul>
             </div>
-        </footer>
-);
+          </div>
+
+          <div className="col-md-3 col-sm-6 col-xs-1">
+            <div className="widget">
+              <h3>INFORMATION</h3>
+
+              <ul>
+                <li>
+                  <a
+                    to="https://lasthorde.com/docs/Whitepaper-V1.11.pdf"
+                    target="_blank"
+                  >
+                    Whitepaper
+                  </a>
+                </li>
+
+                <li>
+                  <a to="https://lasthorde.com/privacy-policy/" target="_blank">
+                    Privacy Policy
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="col-md-3 col-sm-6 col-xs-1">
+            <div className="widget">
+              <h3>FOLLOW US</h3>
+
+              <a
+                to="https://www.youtube.com/channel/UCiCG9a0u-DvJR8HoudRXDdg"
+                target="_blank"
+              >
+                <i className="fa fa-youtube fa-lg"></i>
+              </a>
+
+              <a to="https://t.me/lasthorde" target="_blank">
+                <i className="fa fa-telegram fa-lg"></i>
+              </a>
+
+              <a to="https://twitter.com/LastHorde" target="_blank">
+                <i className="fa fa-twitter fa-lg"></i>
+              </a>
+
+              <a to="https://www.facebook.com/lasthordegame/" target="_blank">
+                <i className="fa fa-facebook fa-lg"></i>
+              </a>
+
+              <a
+                to="https://www.linkedin.com/company/last-horde/"
+                target="_blank"
+              >
+                <i className="fa fa-linkedin fa-lg"></i>
+              </a>
+
+              <a to="https://medium.com/@lasthorde/" target="_blank">
+                <i className="fa fa-medium fa-lg"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="subfooter">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="de-flex">
+                <div className="de-flex-col">
+                  <span>
+                    <span className="contract">
+                      HOR Contract:
+                      <img
+                        width="24"
+                        height="24"
+                        src="/logo-1.png"
+                        alt="HOR Contract"
+                      />
+                      0x9536…0F1A7fD
+                      <FaCopy onClick={handleCopyClipboard}/>
+                      <img
+                        width="24"
+                        height="24"
+                        src="/metamask-icon.png"
+                        alt="Add to Metamask"
+                      />
+                    </span>
+                  </span>
+                </div>
+
+                <div className="de-flex-col">
+                  <span>
+                    <span className="copy">
+                      &copy; Copyright Last Horde. All rights reserved.
+                    </span>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 export default footer;
