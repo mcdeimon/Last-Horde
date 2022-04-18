e306f5d47bc611bdf63f88a6560e25bf15106aad
“ArenasAgustin” <arenasagustin7@gmail.com>
2022-04-18 15:37:12 -0300

    fix arrows

--- client/src/components/components/ArrowCarrousel.js
+++ client/src/components/components/ArrowCarrousel.js
@@ -1,13 +1,13 @@
 const Arrow = ({ className, style, onClick, leftOrRighr, isBlack }) => {
   return (
     <div
-      className={`${className} arrowCarrousel ${leftOrRighr} ${
-        isBlack ? "black" : ""
-      }`}
+      className={`arrowCarrousel ${leftOrRighr} ${isBlack ? "black" : ""}`}
       style={style}
       onClick={onClick}
     >
-      <img src="/img/right-arrow.png" alt="arrow" className="imgArrow"/>
+      <div>
+        <img src="/img/right-arrow.png" alt="arrow" className="imgArrow" />
+      </div>
     </div>
   );
 };
