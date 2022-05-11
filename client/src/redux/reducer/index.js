ae9d43a71e2a38d7eac27968a5abd40ade17ae1a
“ArenasAgustin” <arenasagustin7@gmail.com>
2022-05-11 09:51:16 -0300

    fix documentation

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -16,25 +16,25 @@ import {
 
 const initialState = {
   contract: "",
-  isLoading: true,
+  isLoading: true, // Loading state
 
-  typeExplorer: "sale", //=nfts or packages
+  typeExplorer: "sale", // Nfts or packages
   packages: [],
-  nfts: [], // all nfts
-  filteredNfts: [], // filtered by rarity and type all nfts
+  nfts: [], // All nfts
+  filteredNfts: [], // Filtered by rarity and type all nfts
 
-  account: "", // account address
-  myNfts: [], // my nfts
-  filteredMyNfts: [], // filtered by rarity and type my nfts
-  myFavorites: [], // my favorites
-  myOnSales: [], // my on sales
+  account: "", // Account address
+  myNfts: [], // My nfts
+  filteredMyNfts: [], // Filtered by rarity and type my nfts
+  myFavorites: [], // My favorites
+  myOnSales: [], // My on sales
 
-  nft: {}, //detail of nft
-  package: {}, //detail of package
+  nft: {}, // Detail of nft
+  package: {}, // Detail of package
 
-  rarity: [], //all rarity
+  rarity: [], // All rarity
 
-  onSale: [], //all on sale
+  onSale: [], // All on sale
 };
 
 export const rootReducer = (state = initialState, { type, payload }) => {
@@ -44,7 +44,7 @@ export const rootReducer = (state = initialState, { type, payload }) => {
     case IS_LOADING:
       return {
         ...state,
-        isLoading: payload, // set the loading
+        isLoading: payload, // Set the loading
       };
 
     ////////////////////////////////////////////////////////////// get cards and packages
@@ -52,39 +52,39 @@ export const rootReducer = (state = initialState, { type, payload }) => {
     case GET_ALL_NFT:
       return {
         ...state,
-        nfts: payload, // set the nfts
-        filteredNfts: payload, // set the filtered nfts
+        nfts: payload, // Set the nfts
+        filteredNfts: payload, // Set the filtered nfts
       };
 
     case GET_NFT_BY_ID:
       return {
         ...state,
-        nft: payload, // set the nft
+        nft: payload, // Set the nft
       };
 
     case GET_PACKAGES:
       return {
         ...state,
-        packages: payload, // set the packages
+        packages: payload, // Set the packages
       };
 
     case GET_PACKAGE_BY_ID:
       return {
         ...state,
-        package: payload, // set the package
+        package: payload, // Set the package
       };
 
     case GET_RARITY:
       return {
         ...state,
-        rarity: payload, // set the rarity
+        rarity: payload, // Set the rarity
       };
 
     case GET_ON_SELL:
       let myOnSales = [];
 
       if (state.account)
-        // filter by account
+        // Filter by account
         myOnSales = payload.filter(
           (nft) =>
             nft.account.toLocaleLowerCase() ===
@@ -94,60 +94,60 @@ export const rootReducer = (state = initialState, { type, payload }) => {
 
       return {
         ...state,
-        onSale: payload, // set the on sale
-        myOnSales: myOnSales, // set the my on sale
+        onSale: payload, // Set the on sale
+        myOnSales: myOnSales, // Set the my on sale
       };
 
     ////////////////////////////////////////////////////////////// filters
 
     case FILTER_RARITY:
-      let filteredNftsAux = []; // filtered nfts aux
+      let filteredNftsAux = []; // Filtered nfts aux
 
       switch (payload) {
         case "silver":
           filteredNftsAux = [...state.nfts].filter(
             (nft) => state.rarity[nft.id] === "1"
-          ); // filter by rarity 1
+          ); // Filter by rarity 1
           break;
 
         case "green":
           filteredNftsAux = [...state.nfts].filter(
             (nft) => state.rarity[nft.id] === "2"
-          ); // filter by rarity 2
+          ); // Filter by rarity 2
           break;
 
         case "gold":
           filteredNftsAux = [...state.nfts].filter(
             (nft) => state.rarity[nft.id] === "3"
-          ); // filter by rarity 3
+          ); // Filter by rarity 3
           break;
 
         case "red":
           filteredNftsAux = [...state.nfts].filter(
             (nft) => state.rarity[nft.id] === "4"
-          ); // filter by rarity 4
+          ); // Filter by rarity 4
           break;
 
         case "purple":
           filteredNftsAux = [...state.nfts].filter(
             (nft) => state.rarity[nft.id] === "5"
-          ); // filter by rarity 5
+          ); // Filter by rarity 5
           break;
 
-        default: // filter by all rarity
+        default: // Filter by all rarity
           filteredNftsAux = [...state.nfts];
           break;
       }
 
       return {
         ...state,
-        filteredNfts: filteredNftsAux,
+        filteredNfts: filteredNftsAux, // Set the filtered nfts
       };
 
     case FILTER_TYPE:
       return {
         ...state,
-        typeExplorer: payload ? payload : "sale", // set the type
+        typeExplorer: payload ? payload : "sale", // Set the type
       };
 
     case FILTER_NAME:
@@ -157,7 +157,7 @@ export const rootReducer = (state = initialState, { type, payload }) => {
           if (payload.length >= 3)
             return nft.name.toLowerCase().includes(payload.toLowerCase());
           else return true;
-        }), // filter by name
+        }), // Filter by name
       };
 
     ////////////////////////////////////////////////////////////// account
@@ -165,34 +165,34 @@ export const rootReducer = (state = initialState, { type, payload }) => {
     case GET_ACCOUNT:
       return {
         ...state,
-        account: payload.account, // set the account
-        myNfts: payload.deck, // set the my nfts
-        filteredMyNfts: payload.deck, // set the filtered my nfts
-        myFavorites: payload.favorites, // set the my favorites
+        account: payload.account, // Set the account
+        myNfts: payload.deck, // Set the my nfts
+        filteredMyNfts: payload.deck, // Set the filtered my nfts
+        myFavorites: payload.favorites, // Set the my favorites
         myOnSales: state.onSale.filter(
           (nft) =>
             nft.account.toLocaleLowerCase() ===
             payload.account.toLocaleLowerCase()
-        ), // set the my on sales
+        ), // Set the my on sales
       };
 
     case RESET_ACCOUNT:
       return {
         ...state,
-        account: payload.account, // set the account
-        myNfts: payload.deck, // set the my nfts
-        filteredMyNfts: payload.deck, // set the filtered my nfts
-        myFavorites: payload.favorites, // set the my favorites
-        myOnSales: payload.OnSales, // set the my on sales
+        account: payload.account, // Set the account
+        myNfts: payload.deck, // Set the my nfts
+        filteredMyNfts: payload.deck, // Set the filtered my nfts
+        myFavorites: payload.favorites, // Set the my favorites
+        myOnSales: payload.OnSales, // Set the my on sales
       };
 
     case GET_MY_FAVORITES:
       return {
         ...state,
-        myFavorites: payload, // set the my favorites
+        myFavorites: payload, // Set the my favorites
       };
 
     default:
-      return state; // return the state
+      return state; // Return the state
   }
 };
